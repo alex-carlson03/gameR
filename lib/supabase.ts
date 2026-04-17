0f9f17ac0a7ba4d2166009e3fc31de5394930d2b
sssalve <alexandercarlson03@gmail.com>
2026-04-17 10:06:23 -0600

    Fixed supabase
    oops

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qouuvvtelxsyujmlygrd.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvdXV2dnRlbHhzeXVqbWx5Z3JkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NTE1NDgsImV4cCI6MjA5MTUyNzU0OH0.tXnmv3p2fHOf-xLxwLBPZsuuEqRFiY3H1fM0eAguwHg";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
